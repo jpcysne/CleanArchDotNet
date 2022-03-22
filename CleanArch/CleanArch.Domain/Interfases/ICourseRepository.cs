ff1a5700911b80e0c4eaa01e4b85a7bdc8a3d5c6
João Rubio <jrubio@alter-solutions.com>
2022-03-22 16:53:31 +0100

    Commit Service and Repository Interfases, CourseViewModel

---
+++ CleanArch/CleanArch.Domain/Interfases/ICourseRepository.cs
@@ -0,0 +1,10 @@
+﻿using CleanArch.Domain.Models;
+using System.Collections.Generic;
+
+namespace CleanArch.Domain.Interfases
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
