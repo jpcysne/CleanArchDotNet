6c914e10550513a8ac919024aaf5d7a0cad1945d
João Rubio <jrubio@alter-solutions.com>
2022-03-29 17:40:17 +0200

    Criaçao da InMemoryBus

---
+++ CleanArch/CleanArch.Infra.Bus/InMemoryBus.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Bus
+{
+    public sealed class InMemoryBus : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send<T>(command);
+        }
+    }
+}
